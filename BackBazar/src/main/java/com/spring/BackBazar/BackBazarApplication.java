f26f1231da4e086a8e5d8299b771c1d464dd859f
gastonfalena <gastonfalena@gmail.com>
2024-10-29 17:19:49 -0300

    first commit

---
+++ BackBazar/src/main/java/com/spring/BackBazar/BackBazarApplication.java
@@ -0,0 +1,13 @@
+package com.spring.BackBazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBazarApplication.class, args);
+	}
+
+}
---
+++ BackBazar/src/test/java/com/spring/BackBazar/BackBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.BackBazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
